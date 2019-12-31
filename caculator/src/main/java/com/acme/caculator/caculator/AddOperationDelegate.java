c9cbc95c9999a532174d163592bc8a58fe19944e
Little Leo <34904228+yanshi726@users.noreply.github.com>
2019-12-30 20:12:48 -0500

    Add files via upload

---
+++ caculator/src/main/java/com/acme/caculator/caculator/AddOperationDelegate.java
@@ -0,0 +1,22 @@
+package com.acme.caculator.caculator;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+
+@Component("AddOperation")
+public class AddOperationDelegate implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		// TODO Auto-generated method stub
+		Long operA=(Long) execution.getVariable("OperandA");
+		Long operB=(Long) execution.getVariable("OperandB");
+		Long result=operA + operB;
+		
+		execution.setVariable("result", result);
+
+	}
+
+}
---
+++ caculator/src/main/java/com/acme/caculator/caculator/MinusOperationDelegate.java
@@ -0,0 +1,21 @@
+package com.acme.caculator.caculator;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+@Component("MinusOperation")
+public class MinusOperationDelegate implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		// TODO Auto-generated method stub
+		Long operA=(Long) execution.getVariable("OperandA");
+		Long operB=(Long) execution.getVariable("OperandB");
+		Long result=operA - operB;
+		
+		execution.setVariable("result", result);
+
+	}
+
+}
